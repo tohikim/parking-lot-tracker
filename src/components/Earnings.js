0b3045db27d1332e49666c7ca2e71f9d9ececc0a
Tohi Kim <tohikimbusiness@gmail.com>
2026-05-12 19:14:34 +0400

    fix: merged styling to JS file (Entry.js)

--- src/components/Earnings.js
+++ src/components/Earnings.js
@@ -1,16 +1,43 @@
-import './Earnings.css';
-
 function Earnings(props) {
-    return (
-        <div className="earnings-box">
-            <h5>DAY EARNINGS</h5>
-            <p>Seconds rate: $2</p>
-            <div className="total-earnings">
-                <p>Today's earnings: ${' '}</p>
-                <p>{props.totalEarnings}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div style={styles.earningsBox}>
+      <h5 style={styles.h5}>DAY EARNINGS</h5>
+      <p style={styles.p}>Seconds rate: $2</p>
+      <div style={styles.totalEarnings}>
+        <p style={styles.p}>Today's earnings: $ </p>
+        <p style={styles.p}>{props.totalEarnings}</p>
+      </div>
+    </div>
+  );
 }
+
+const styles = {
+  earningsBox: {
+    backgroundColor: "white",
+    border: "1px dotted #7D99AA",
+    borderRadius: "15px",
+    width: "75%",
+    minHeight: "150px",
+    padding: "1rem",
+    color: "#7D99AA",
+  },
+
+  totalEarnings: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 0,
+  },
+
+  h5: {
+    margin: 0,
+    marginBottom: "1rem",
+    fontSize: "1.5rem",
+  },
+  p: {
+    margin: 0,
+    color: "black",
+    textAlign: "left",
+  },
+};
 
 export default Earnings;
