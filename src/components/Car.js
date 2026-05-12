2dc96aceadb37b2d6b65e7b81969dfdac147eff0
Tohi Kim <tohikimbusiness@gmail.com>
2026-05-12 19:08:12 +0400

    fix: merged styling into JS file (Car.js)

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,52 +1,83 @@
-import './Car.css';
 import { differenceInSeconds } from "date-fns";
 import { format } from "date-fns";
 
 function Car(props) {
-    const handleClick = () => {
-        const exitTime = new Date();
+  const handleClick = () => {
+    const exitTime = new Date();
 
-        const exitedCar = {
-            model: props.model,
-            color: props.color,
-            entryTime: props.entryTime,
-            _id: props._id,
-            exitTime: exitTime,
-            duration: differenceInSeconds(exitTime, props.entryTime)
-        }
+    const exitedCar = {
+      model: props.model,
+      color: props.color,
+      entryTime: props.entryTime,
+      _id: props._id,
+      exitTime: exitTime,
+      duration: differenceInSeconds(exitTime, props.entryTime),
+    };
 
-        props.setExitedCars((prevState) => {
-            return [...prevState, exitedCar];
-        })
+    props.setExitedCars((prevState) => {
+      return [...prevState, exitedCar];
+    });
 
-        props.setTotalEarnings((prevState) => {
-            const currentRate = exitedCar.duration * 2;
-            const totalRate = prevState + currentRate;
-            return totalRate;
-        })
+    props.setTotalEarnings((prevState) => {
+      const currentRate = exitedCar.duration * 2;
+      const totalRate = prevState + currentRate;
+      return totalRate;
+    });
 
-        props.setParkedCars((prevState) => {
-            const filteredArray = prevState.filter((parkedCar) => parkedCar._id !== props._id);
-            return filteredArray;
-        })
-    }
-    return (
-        <div className="car-container">
-            <div className="car-content">
-                <p>Model:{' '}</p>
-                <p>{props.model}</p>
-            </div>
-            <div className="car-content">
-                <p>Color:{' '}</p>
-                <p>{props.color}</p>
-            </div>
-            <div className="car-content">
-                <p>Entry time:{' '}</p>
-                <p>{format(props.entryTime, 'yyyy-MM-dd HH:mm:ss')}</p>
-            </div>
-            <button className="button-two" onClick={handleClick}>EXIT</button>
-        </div>
-    )
+    props.setParkedCars((prevState) => {
+      const filteredArray = prevState.filter(
+        (parkedCar) => parkedCar._id !== props._id,
+      );
+      return filteredArray;
+    });
+  };
+  return (
+    <div style={styles.carContainer}>
+      <div style={styles.carContent}>
+        <p style={styles.p}>Model: </p>
+        <p style={styles.p}>{props.model}</p>
+      </div>
+      <div style={styles.carContent}>
+        <p style={styles.p}>Color: </p>
+        <p style={styles.p}>{props.color}</p>
+      </div>
+      <div style={styles.carContent}>
+        <p style={styles.p}>Entry time: </p>
+        <p style={styles.p}>{format(props.entryTime, "yyyy-MM-dd HH:mm:ss")}</p>
+      </div>
+      <button style={styles.buttonTwo} onClick={handleClick}>
+        EXIT
+      </button>
+    </div>
+  );
 }
+
+const styles = {
+  carContainer: {
+    backgroundImage: 'url("../assets/car-icon.png")',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    borderRadius: "15px",
+    maxHeight: "130px",
+    maxWidth: "350px",
+    padding: "1rem",
+  },
+
+  carContent: {
+    display: "flex",
+    flexDirection: "row",
+    gap: "10px",
+  },
+
+  p: {
+    margin: 0,
+    paddingBottom: "0.5rem",
+    color: "white",
+  },
+
+  buttonTwo: {
+    padding: "0.3rem",
+  },
+};
 
 export default Car;
