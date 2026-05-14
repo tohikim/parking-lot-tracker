b7a6372c305f3b5e995fffac7c84df9eb0f51385
Tohi Kim <tohikimbusiness@gmail.com>
2026-05-14 14:08:20 +0400

    feat(app): merged remaining css to js files

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,15 +24,17 @@ function Header(props) {
     }
   };
 
+  const [isHovered, setIsHovered] = useState(false);
+
   const [model, setModel] = useState("default");
   const [color, setColor] = useState("");
 
   return (
-    <div className="header-container">
-      <h1>PARKING LOT TRACKER</h1>
-      <div className="instruction">
-        <h3>Click to add car details!</h3>
-        <form onSubmit={handleOnSubmit}>
+    <div style={styles.headerContainer}>
+      <h1 style={styles.h1}>PARKING LOT TRACKER</h1>
+      <div style={styles.instruction}>
+        <h3 style={styles.h3}>Click to add car details!</h3>
+        <form onSubmit={handleOnSubmit} style={styles.form}>
           <label>
             Model:{" "}
             <select value={model} onChange={(e) => setModel(e.target.value)}>
@@ -50,7 +52,16 @@ function Header(props) {
               onChange={(e) => setColor(e.target.value)}
             />
           </label>
-          <button className="button-one" type="submit">
+          <button
+            type="submit"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            style={{
+              ...styles.button,
+              backgroundColor: isHovered ? "#FFC067" : "#7D99AA",
+              cursor: "pointer",
+            }}
+          >
             ADD CAR
           </button>
         </form>
@@ -58,5 +69,54 @@ function Header(props) {
     </div>
   );
 }
+
+const styles = {
+  headerContainer: {
+    textAlign: "center",
+    padding: "2%",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: "1.4rem",
+  },
+  instruction: {
+    border: "1px dotted #7D99AA",
+    padding: "1rem",
+    borderRadius: "15px",
+  },
+  form: {
+    borderRadius: "5%",
+    gap: "1rem",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "left",
+    textAlign: "left",
+    maxWidth: "300px",
+  },
+
+  h1: {
+    fontSize: "3rem",
+    color: "#66C4FF",
+    marginBottom: "1rem",
+  },
+
+  h3: {
+    fontSize: "1.2rem",
+    margin: "0px",
+    marginBottom: "1rem",
+    color: "#7D99AA",
+  },
+  button: {
+    border: "1px",
+    borderRadius: "10px",
+    backgroundColor: "#7D99AA",
+    color: "whitesmoke",
+    fontSize: "1rem",
+    fontWeight: "700",
+    width: "100%",
+    padding: "1rem",
+  },
+};
 
 export default Header;
--- src/components/ParkingLot.jsx
+++ src/components/ParkingLot.jsx
@@ -1,16 +1,39 @@
-import './ParkingLot.css';
-import Car from './Car.js';
+import Car from "./Car.js";
 
 function ParkingLot(props) {
-    return (
-        <div className="parking-container">
-            {props.parkedCars.map((parkedCar) => {
-                    return (
-                        <Car model={parkedCar.model} color={parkedCar.color} entryTime={parkedCar.entryTime} _id={parkedCar._id} setExitedCars={props.setExitedCars} setParkedCars={props.setParkedCars} setTotalEarnings={props.setTotalEarnings}/>
-                    );
-            })}
-        </div>
-    )
+  return (
+    <div style={styles.parkingContainer}>
+      {props.parkedCars.map((parkedCar) => {
+        return (
+          <Car
+            model={parkedCar.model}
+            color={parkedCar.color}
+            entryTime={parkedCar.entryTime}
+            _id={parkedCar._id}
+            setExitedCars={props.setExitedCars}
+            setParkedCars={props.setParkedCars}
+            setTotalEarnings={props.setTotalEarnings}
+          />
+        );
+      })}
+    </div>
+  );
 }
+
+const styles = {
+  parkingContainer: {
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    border: "10px dashed yellow",
+    borderRadius: "25px",
+    backgroundColor: "gray",
+    minHeight: "250px",
+    height: "510px",
+    gap: "2rem",
+    padding: "2rem",
+    marginBottom: "3%",
+  },
+};
 
 export default ParkingLot;
